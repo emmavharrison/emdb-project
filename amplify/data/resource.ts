38e9cd184d613a3be66d689a8d44f5afe866d5e8
Emma Harrison <emma.harrison.98@outlook.com>
2025-01-23 14:46:59 +0000

    start backend setup

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,14 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  MovieDB: a
     .model({
-      content: a.string(),
+      userId: a.string(),
+      collectionId: a.string(),
+      movieId: a.string(),
+      reviewId: a.string(),
+      reviewText: a.string(),
+      collectionName: a.string()
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -27,12 +32,13 @@ export const data = defineData({
   },
 });
 
+
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
